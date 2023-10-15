a32e3079c2d6e110c3e347b64afd5f8108f4828a
Timothy Serem <kipkorirserem3@gmail.com>
2023-10-15 22:38:42 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,26 @@ const initialize = async () => {
   const device: GPUDevice | undefined = await adapter?.requestDevice();
   const context: GPUCanvasContext | null = canvas.getContext("webgpu");
   const format: GPUTextureFormat = "bgra8unorm";
+
   if (device && context) {
     context.configure({
       device: device,
       format: format,
     });
+
+    const bindGroupLayout: GPUBindGroupLayout = device.createBindGroupLayout({
+      entries: [],
+    });
+
+    const bindGroup: GPUBindGroup = device.createBindGroup({
+      layout: bindGroupLayout,
+      entries: [],
+    });
+
+    const pipelineLayout: GPUPipelineLayout = device.createPipelineLayout({
+      bindGroupLayouts: [bindGroupLayout],
+    });
+
     const pipeline: GPURenderPipeline = device.createRenderPipeline({
       vertex: {
         module: device.createShaderModule({
@@ -45,12 +60,15 @@ const initialize = async () => {
         topology: "triangle-list",
       },
 
-      layout: "auto",
+      layout: pipelineLayout,
     });
+
     const commandEncoder: GPUCommandEncoder = device.createCommandEncoder();
+
     const textureView: GPUTextureView = context
       .getCurrentTexture()
       .createView();
+
     const renderPass: GPURenderPassEncoder = commandEncoder.beginRenderPass({
       colorAttachments: [
         {
@@ -61,9 +79,12 @@ const initialize = async () => {
         },
       ],
     });
+
     renderPass.setPipeline(pipeline);
+    renderPass.setBindGroup(0, bindGroup);
     renderPass.draw(3, 1, 0, 0);
     renderPass.end();
+
     device.queue.submit([commandEncoder.finish()]);
   } else {
     alert("Failed to initialize WebGPU context");
